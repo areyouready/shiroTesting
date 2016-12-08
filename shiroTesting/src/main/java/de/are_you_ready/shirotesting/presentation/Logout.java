dc5b11ac012326215fe73f3efe5ba59b2652c65b
Sebastian Basner <sebastian.basner@gmx.de>
2016-12-08 11:12:42 +0100

    Added sample for programmatic role check and site restriction for roles

--- shiroTesting/src/main/java/de/are_you_ready/shirotesting/presentation/Logout.java
+++ shiroTesting/src/main/java/de/are_you_ready/shirotesting/presentation/Logout.java
@@ -19,10 +19,12 @@ public class Logout {
      * Shiro logout for the current user
      */
     public void submit() throws IOException {
-        final ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-        SecurityUtils.getSubject().logout();
-        externalContext.invalidateSession();  // cleanup user related session state
-        externalContext.redirect("login.xhtml?faces-redirect=true");
+        if (SecurityUtils.getSubject().hasRole("root")) {
+            final ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+            SecurityUtils.getSubject().logout();
+            externalContext.invalidateSession();  // cleanup user related session state
+            externalContext.redirect("login.xhtml?faces-redirect=true");
+        }
     }
 
 }
